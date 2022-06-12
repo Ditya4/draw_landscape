6e669a2f14aad6f769dd8f513917c658f60dc5b6
Ditya4 <72632156+Ditya4@users.noreply.github.com>
2022-06-12 18:41:02 +0300

    add clouds

--- draw_landscape.py
+++ draw_landscape.py
@@ -2,6 +2,37 @@ import pygame
 from random import randint
 
 
+class Cloud:
+
+    def __init__(self, x, y, width, height, x_speed, y_speed, surface, color):
+        """
+            x, y are a coords of left top of the cloud rect, which is out
+            of the ellipse figure
+        """
+        self.x = x
+        self.cloud_x = x
+        self.y = y
+        self.cloud_y = y
+        self.width = width
+        self.height = height
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+        self.surface = surface
+        self.color = color
+
+    def draw_cloud(self):
+        pygame.draw.ellipse(self.surface, self.color,
+                            (self.x, self.y, self.width, self.height))
+
+    def move_cloud(self, window_width):
+        if self.x <= window_width:
+            self.x += self.x_speed
+        else:
+            self.x = self.cloud_x
+            self.y = self.cloud_y
+        self.y += self.y_speed
+
+
 class Mushroom:
 
     def __init__(self, x, y, hat_width, hat_height, stalk_width, stalk_height,
@@ -30,7 +61,8 @@ class Mushroom:
                             mushroom_stalk_points)
 
     def draw_hat(self):
-        ellipse_points = ((self.x + self.stalk_width // 2 - self.hat_width // 2,
+        ellipse_points = ((self.x + self.stalk_width // 2 -
+                           self.hat_width // 2,
                            self.y - self.stalk_height - self.hat_height,
                            self.hat_width, self.hat_height))
         pygame.draw.ellipse(self.surface, self.hat_color, ellipse_points)
@@ -89,7 +121,7 @@ class House:
         pygame.draw.polygon(self.surface, self.window_color, window_points)
         # draw_points(window_points, self.surface)
 
-    def draw_shadow_walls(self, sun, lenght):
+    def draw_shadow_house(self, sun, lenght):
         """
             left bottom and right bottom points of the wall are used for shadow
             polygone and left top and right top will be send into
@@ -237,7 +269,7 @@ def bg_draw(surface, color, points):
     """
         Draw background into black color and white snow in front
     """
-    surface.fill((0, 0, 0,))
+    surface.fill(("cyan"))
     pygame.draw.polygon(surface, color, (points))
 
 
@@ -264,7 +296,8 @@ def main():
     sun_x_speed = 5
     sun_y_speed = 1
     right_horizont_y = 350
-    snow_first_point = (-3, 250)
+    left_horizont_y = 250
+    snow_first_point = (-3, left_horizont_y)
     snow_second_point = (602, right_horizont_y)
     snow_third_point = (602, 602)
     snow_fourth_point = (-3, 602)
@@ -274,8 +307,8 @@ def main():
     stick_height = 90
     stick_color = "brown"
     stick_shadow_length = 50
-    house_x = 120
-    house_y = 330
+    house_x = 320
+    house_y = 380
     house_width = 60
     house_height = 80
     house_walls_color = "cyan"
@@ -293,7 +326,7 @@ def main():
                          "firebrick1")
     tree_crown_color_index = 0
     tree_crown_size = 4
-    trees_count = 5
+    trees_count = 30
     trees = []
     # mushroom_x = 400
     # mushroom_y = 400
@@ -304,8 +337,17 @@ def main():
     mushroom_stalk_height = 16
     mushroom_hat_colors = ("red", "green")
     mushroom_stalk_color = "brown"
-    mushrooms_count = 8
+    mushrooms_count = 30
     mushrooms = []
+    cloud_x = 30
+    cloud_y = 30
+    cloud_width = 60
+    cloud_height = 25
+    cloud_x_speed = 5
+    cloud_y_speed = 1
+    cloud_colors = ("white", "MistyRose2", "PaleGreen1")
+    clouds_count = 50
+    clouds = []
     points = (snow_first_point, snow_second_point, snow_third_point,
               snow_fourth_point)
     run = True
@@ -337,12 +379,19 @@ def main():
         mushrooms.append(Mushroom(randint(tree_lower_segment_width // 2,
                                   window_width -
                                   tree_lower_segment_width // 2),
-                          randint(right_horizont_y, window_height),
-                          mushroom_hat_width,
-                          mushroom_hat_height, mushroom_stalk_width,
-                          mushroom_stalk_height, win,
-                          mushroom_hat_colors[mushroom_hat_color_index],
-                          mushroom_stalk_color))
+                         randint(right_horizont_y, window_height),
+                         mushroom_hat_width,
+                         mushroom_hat_height, mushroom_stalk_width,
+                         mushroom_stalk_height, win,
+                         mushroom_hat_colors[mushroom_hat_color_index],
+                         mushroom_stalk_color))
+
+    for number in range(clouds_count):
+        cloud_color_index = number % len(cloud_colors)
+        clouds.append(Cloud(randint(-window_width, -cloud_width),
+                      randint(-cloud_height, left_horizont_y - cloud_height * 6),
+                      cloud_width, cloud_height, cloud_x_speed,
+                      cloud_y_speed, win, cloud_colors[cloud_color_index]))
 
     while run:
         clock.tick(10)
@@ -351,16 +400,22 @@ def main():
                 run = False
 
         bg_draw(win, "white", points)
+
         sun.move(window_width, sun_x, sun_y)
         sun.draw()
+        for cloud in clouds:
+            cloud.move_cloud(window_width)
+            cloud.draw_cloud()
         stick.draw()
         stick.draw_shadow(sun, stick_shadow_length)
+        house.draw_shadow_house(sun, house_shadow_length)
         house.draw_house()
-        house.draw_shadow_walls(sun, house_shadow_length)
         for tree in trees:
             tree.draw_tree()
         for mushroom in mushrooms:
             mushroom.draw_mushroom()
+
+
         pygame.display.update()
     pygame.quit()
 
